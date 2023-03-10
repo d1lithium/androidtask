966db43f0ba95a1c9463042deddf0c0e5ec034d6
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2023-03-10 10:19:36 +0500

    Implemented QRCode feature and tested

--- app/src/main/java/com/verodigital/androidtask/presentation/ui/activitities/MainActivity.kt
+++ app/src/main/java/com/verodigital/androidtask/presentation/ui/activitities/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.verodigital.androidtask.presentation.ui.activitities
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -12,26 +17,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.Navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.findNavController
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import com.verodigital.androidtask.R
+import com.verodigital.androidtask.databinding.ActivityMainBinding
 import com.verodigital.androidtask.domain.TaskListViewModel
 import com.verodigital.androidtask.presentation.ui.theme.AndroidtaskTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collectLatest
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     val b = Bundle()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nav_host_fragment_container.findNavController().navigate(R.id.mainFragment,b)
 
+
     }
+
+
 
 }
--- app/src/main/java/com/verodigital/androidtask/presentation/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/verodigital/androidtask/presentation/ui/fragments/MainFragment.kt
@@ -1,8 +1,13 @@
 package com.verodigital.androidtask.presentation.ui.fragments
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toLowerCase
@@ -11,6 +16,10 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import com.verodigital.androidtask.R
 import com.verodigital.androidtask.data.datasource.Task
 import com.verodigital.androidtask.domain.TaskListViewModel
@@ -33,6 +42,16 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private var v: View? = null
     private var populateTaskJob0: Job? = null
     private var populateTaskJob1: Job? = null
+    val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_AZTEC
+        )
+        .build()
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+    private var imgBitmap: Bitmap? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -42,7 +61,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launch(Dispatchers.Main) {
             populateList()
-           // populateListFromLocalDB()
+            // populateListFromLocalDB()
 
         }
         setTaskAdapter()
@@ -58,6 +77,10 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         })
 
+        qrcodeBtn.setOnClickListener(View.OnClickListener {
+            scanImg()
+        })
+
     }
 
     private fun filterTaskList(query: String?) {
@@ -68,12 +91,23 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 i.description?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
                 i.sort?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
                 i.wageType?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
-                i.BusinessUnitKey?.let { i.BusinessUnitKey!!.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!!} == true ||
-                i.businessUnit?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
-                i.parentTaskID?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
-                i.preplanningBoardQuickSelect?.let { i.preplanningBoardQuickSelect!!.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!!} == true ||
+                i.BusinessUnitKey?.let {
+                    i.BusinessUnitKey!!.lowercase(java.util.Locale.ROOT)
+                        ?.contains(query?.lowercase()!!)!!
+                } == true ||
+                i.businessUnit?.lowercase(java.util.Locale.ROOT)
+                    ?.contains(query?.lowercase()!!)!! ||
+                i.parentTaskID?.lowercase(java.util.Locale.ROOT)
+                    ?.contains(query?.lowercase()!!)!! ||
+                i.preplanningBoardQuickSelect?.let {
+                    i.preplanningBoardQuickSelect!!.lowercase(java.util.Locale.ROOT)
+                        ?.contains(query?.lowercase()!!)!!
+                } == true ||
                 i.colorCode?.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!! ||
-                i.workingTime?.let { i.workingTime!!.lowercase(java.util.Locale.ROOT)?.contains(query?.lowercase()!!)!!} == true ||
+                i.workingTime?.let {
+                    i.workingTime!!.lowercase(java.util.Locale.ROOT)
+                        ?.contains(query?.lowercase()!!)!!
+                } == true ||
                 i.isAvailableInTimeTrackingKioskMode.toString()?.lowercase(java.util.Locale.ROOT)
                     .contains(query?.lowercase()!!)
             ) {
@@ -84,7 +118,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     }
 
-    private suspend fun populateList(){
+    private suspend fun populateList() {
         populateTaskJob0?.cancel()
         populateTaskJob0 = lifecycleScope.launch {
             taskListViewModel.getAllLocalTasks().collectLatest {
@@ -92,10 +126,9 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             }
         }
         populateTaskJob1 = lifecycleScope.launch {
-            if (taskList.isNotEmpty()){
+            if (taskList.isNotEmpty()) {
                 taskAdapter.updateTasks(taskList)
-            }
-            else {
+            } else {
 
                 taskListViewModel.getAllTasks().collectLatest {
                     taskAdapter.updateTasks(it)
@@ -125,10 +158,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         populateTaskJob1?.join()
 
 
-
-
-
-
     }
 
     private suspend fun populateListFromLocalDB() {
@@ -139,11 +168,107 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     }
 
     private fun setTaskAdapter() {
-            recyclerViewTask.apply {
-                layoutManager = LinearLayoutManager(context)
-                adapter = taskAdapter
-            }
+        recyclerViewTask.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = taskAdapter
+        }
     }
+
+
+    private fun scanImg() {
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+        } catch (e: Exception) {
+
+            Toast.makeText(
+                activity, e.message,
+                Toast.LENGTH_LONG
+            ).show();
+
+        }
+
+
+    }
+
+    private fun processScan() {
+        if (imgBitmap != null) {
+
+            val mImg = InputImage.fromBitmap(imgBitmap!!, 0)
+            val mScanner = BarcodeScanning.getClient(options)
+
+            mScanner.process(mImg).addOnSuccessListener { qrcodes ->
+
+                    if (qrcodes.toString() == "[]") {
+                        Toast.makeText(
+                            activity, "nothing to scan",
+                            Toast.LENGTH_LONG
+                        ).show();
+
+                    }
+
+
+                    for (qrcode in qrcodes) {
+                        val qrcodeType = qrcode.valueType
+                        when (qrcodeType) {
+
+                            Barcode.TYPE_TEXT -> {
+                                qrcode.rawValue?.let {
+                                    val text = qrcode.rawValue!!.toString()
+                                    taskSearchView.setQuery(text, true)
+                                }
+
+                            }
+
+                        }
+                    }
+
+
+                }
+                .addOnFailureListener {
+
+                    Toast.makeText(
+                        activity, "QRCode scanning failed",
+                        Toast.LENGTH_LONG
+                    ).show();
+
+                }
+
+
+        } else {
+
+
+            Toast.makeText(
+                activity, "No image selected to scan for QR",
+                Toast.LENGTH_LONG
+            ).show();
+
+
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
+
+            val extras: Bundle? = data?.extras
+            imgBitmap = extras?.get("data") as Bitmap
+            imgBitmap?.let {
+                processScan()
+            }
+
+        }
+
+
+    }
+
 
 }
 
